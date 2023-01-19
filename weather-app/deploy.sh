c8574f44a5484afb46731288d2af57c94d09431a
viky <112059651+vikyw89@users.noreply.github.com>
2023-01-19 10:48:20 +0000

    checkpoint

--- weather-app/deploy.sh
+++ weather-app/deploy.sh
@@ -21,9 +21,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:vikyw89/vikyw89.github.io.git main
+# git push -f git@github.com:vikyw89/vikyw89.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f git@github.com:vikyw89/weatherApp.git main:gh-pages
 
 cd -
